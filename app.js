ba6dc259e1853c01111ec30b7d713b8b02b7aa5c
vasanthvk47 <vasanth472004@gmail.com>
2025-04-26 05:14:58 +0000

    Initial project commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.end('Hello from Jenkins to Kubernetes 🚀');
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
